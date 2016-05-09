41032e4969e23515076a5c27a804c78ace8665cf
Megan Nilles <darkstarre@hotmail.com>
2016-05-09 11:38:54 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  
+  resources :beers do
+    resources :reviews
+  end
+  
+  root 'welcome#index'
 
+  get '/beers/:beer_id/reviews/:id' => 'reviews#show'
+
+
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
