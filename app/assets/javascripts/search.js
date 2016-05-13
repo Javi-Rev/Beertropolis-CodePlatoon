890c72966ea098b9c762f23b3bbdd8ce19a28ff9
Megan Nilles <darkstarre@hotmail.com>
2016-05-13 10:24:12 -0500

    fixed search to return to redirect

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,20 +1,38 @@
-var onReady = function() {
+var substringMatcher = function(strs) {
+  return function findMatches(query, callback) {
+    var matches, substringRegex;
 
-  var beers = new Bloodhound ({
-    datumTokenizer: Bloodhound.tokenizers.whitespace,
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: '../../../db/beers.json'
-  });
+    // an array that will be populated with substring matches
+    matches = [];
 
-  beers.initialize();
+    // regex used to determine if a string contains the substring `query`
+    substrRegex = new RegExp(query, 'i');
 
-  $('#prefetch .typeahead').typeahead(null, {
-      name: 'name'
-      source: beers
-});
+    // iterate through the pool of strings and for any string that
+    // contains the substring `query`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
 
-
-  $(searchSelector).bind('typeahead:selected', function(event, datum, name){
-    window.location.href = '/search/'+datum.id;
-  });
+    callback(matches);
+  };
 };
+
+var beerData = gon.Beers
+
+$(function() {
+  $('#the-basics .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: 'beerData',
+    source: substringMatcher(beerData)
+  }).on('typeahead:select', function(event, selection) {
+    console.log("selection: ", selection);
+    window.location = "/localhost:3000/beers/" + (beerData.indexOf(selection)+1);
+  });
+})
