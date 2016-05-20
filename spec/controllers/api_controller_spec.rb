ce1db260eec14bfef10967562dffdb98fc978986
Megan Nilles <darkstarre@hotmail.com>
2016-05-19 21:03:41 -0500

    controller specs

---
+++ spec/controllers/api_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec describe ApiController, :type => :controller do
+  describe "GET beers" do
+    it "renders the beers api" do
+      get :beers
+      expect(response).to render_template("beers")
+    end
+  end
+end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+require 'spec_helper'
+
+Rspec.describe BeersController. :type => :controller do
+  describe "GET index" do
+    it "renders the beers index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET show" do
+    it "renders the beers show template" do
+      get :show
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "GET new" do
+    it "renders the beers new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "GET edit" do
+    it "renders the edit beer template" do
+      get :edit
+      expect(response).to render_template("edit") 
+    end
+  end
+
+ 
+end
