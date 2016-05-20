b2519395bfe47a6adbf761684ee9f6ac705e905b
Megan Nilles <darkstarre@hotmail.com>
2016-05-19 21:23:07 -0500

    controller specs

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ApiController, :type => :controller do
   describe "GET beers" do
     it "renders the beers api" do
       get :beers
-      expect(response).to render_template("beers")
+      expect(response).to render_template("[]")
     end
   end
 end
