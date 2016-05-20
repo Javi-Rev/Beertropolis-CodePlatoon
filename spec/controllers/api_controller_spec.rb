49604c24b484a37c0c6795f8bfd4a86b53599e58
Megan Nilles <darkstarre@hotmail.com>
2016-05-19 21:12:23 -0500

    controller specs

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec describe ApiController, :type => :controller do
+RSpec.describe ApiController, :type => :controller do
   describe "GET beers" do
     it "renders the beers api" do
       get :beers
